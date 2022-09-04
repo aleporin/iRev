f7414974a9ade8c4a9d87a9002257753d3d31480
Austin LePorin <austinleporin@gmail.com>
2022-09-03 23:04:42 -0400

    Cocktail Controller

---
+++ controllers/CocktailController.js
@@ -0,0 +1,20 @@
+const { Cocktail, User } = require('../models')
+
+const CreateCocktail = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    const createdCocktail = await Cocktail.create({
+      cocktail_name: req.body.cocktail_name,
+      desc: req.body.desc,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      cook_time: req.body.cook_time,
+      process: req.body.process,
+      image: req.body.image,
+      userId: userId
+    })
+    res.send(createdCocktail)
+  } catch (e) {
+    throw e
+  }
+}
