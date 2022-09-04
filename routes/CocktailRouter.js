4b10508e28d6a18500fdce5e6beaf5a1239b512a
Austin LePorin <austinleporin@gmail.com>
2022-09-03 23:09:35 -0400

    Create Cocktail Routes

--- routes/CocktailRouter.js
+++ routes/CocktailRouter.js
@@ -5,8 +5,8 @@ const controller = require('../Controllers/CocktailController')
 router.get('/', controller.GetAllCocktails)
 router.get('/user/:userId', controller.GetUserCocktails)
 router.get('/:cocktailId', controller.GetCocktailsById)
-router.get('/update/:cocktailId', controller.UpdateRecipe)
-router.get('/delete/:recipeId', controller.DeleteRecipe)
-router.post('/create/:user_id', controller.CreateRecipe)
+router.get('/update/:cocktailId', controller.UpdateCocktail)
+router.get('/delete/:cocktailId', controller.DeleteCocktail)
+router.post('/create/:user_id', controller.CreateCocktail)
 
 module.exports = router
