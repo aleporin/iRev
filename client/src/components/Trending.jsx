a512521f0605b54edce520014d6edaae6878c8dc
Austin LePorin <austinleporin@gmail.com>
2022-09-05 16:09:59 -0400

    Some Basic Stle

--- client/src/components/Trending.jsx
+++ client/src/components/Trending.jsx
@@ -1,5 +1,7 @@
 import { animationControls } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
 import axios from 'axios'
 
 const API_KEY = process.env.REACT_APP_API_KEY
@@ -10,7 +12,7 @@ const Trending = () => {
   useEffect(() => {
     const getTrending = async () => {
       const response = await axios.get(
-        `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=10`
+        `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=9`
       )
       setTrending(response.data.recipes)
     }
@@ -21,12 +23,24 @@ const Trending = () => {
     <div>
       <div className="card-wrapper">
         <h3>Trending Recipes</h3>
-        {trending.map((recipe) => (
-          <div key={recipe.id} className="card-content">
-            <p>{recipe.title}</p>
-            <img src={recipe.image} />
-          </div>
-        ))}
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem'
+          }}
+        >
+          {trending.map((recipe) => (
+            <SplideSlide>
+              <div key={recipe.id} className="card-content">
+                <p>{recipe.title}</p>
+                <img src={recipe.image} />
+              </div>
+            </SplideSlide>
+          ))}
+        </Splide>
       </div>
     </div>
   )
